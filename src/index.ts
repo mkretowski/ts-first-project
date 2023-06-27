ff17355b5a482ae9ea28089025370104038f68b3
Mateusz Kretowski <matkretkurs@gmail.com>
2023-06-27 15:30:25 +0200

    Change StartApp method and Add update method in UsersData class

--- src/index.ts
+++ src/index.ts
@@ -47,8 +47,14 @@ interface User {
   age: number;
 }
 
+interface updatedUser {
+  name: string;
+  newName: number;
+  newAge: number;
+}
+
 class UsersData {
-  private data: User[] = [];
+  private data: (User | updatedUser)[] = [];
 
   public showAll(): void {
     if (this.data.length === 0) console.log('No data...');
@@ -70,6 +76,21 @@ class UsersData {
       Message.showColorized(MessageVariant.Error, 'Wrong data!');
     }
   }
+  public update(updateUser: updatedUser): void {
+    const index = this.data.findIndex((user) => user.name === updateUser.name);
+    if (
+      index !== -1 &&
+      typeof updateUser.newAge === 'number' &&
+      updateUser.newAge > 0 &&
+      typeof updateUser.name === 'string' &&
+      updateUser.name.length > 0
+    ) {
+      this.data[index] = updateUser;
+      Message.showColorized(MessageVariant.Success, 'User has been successfully updated!');
+    } else {
+      Message.showColorized(MessageVariant.Error, 'Wrong data!');
+    }
+  }
   public remove(userName: string): void {
     const index = this.data.findIndex((user) => user.name === userName);
     if (index !== -1) {
@@ -81,18 +102,32 @@ class UsersData {
   }
 }
 
-const startApp = async () => {
-  enum Action {
-    List = 'list',
-    Add = 'add',
-    Remove = 'remove',
-    Quit = 'quit',
-  }
+const users = new UsersData();
+console.log('\n');
+console.info('???? Welcome to the UsersApp!');
+console.log('====================================');
+Message.showColorized(MessageVariant.Info, 'Available actions');
+console.log('\n');
+console.log('list – show all users');
+console.log('add – add new user to the list');
+console.log('update – update user data on the list');
+console.log('remove – remove user from the list');
+console.log('quit – quit the app');
+console.log('\n');
 
-  type InquirerAnswers = {
-    action: Action;
-  };
+enum Action {
+  List = 'list',
+  Add = 'add',
+  Update = 'update',
+  Remove = 'remove',
+  Quit = 'quit',
+}
 
+type InquirerAnswers = {
+  action: Action;
+};
+
+const startApp = () => {
   inquirer
     .prompt([
       {
@@ -101,9 +136,64 @@ const startApp = async () => {
         message: 'How can I help you?',
       },
     ])
-    .then((answers: InquirerAnswers) => {
-      console.log('Chosen action: ' + answers.action);
-      if (answers.action === 'quit') return;
+    .then(async (answers: InquirerAnswers) => {
+      switch (answers.action) {
+        case Action.List:
+          users.showAll();
+          break;
+        case Action.Add:
+          const user = await inquirer.prompt([
+            {
+              name: 'name',
+              type: 'input',
+              message: 'Enter name',
+            },
+            {
+              name: 'age',
+              type: 'number',
+              message: 'Enter age',
+            },
+          ]);
+          users.add(user);
+          break;
+        case Action.Remove:
+          const name = await inquirer.prompt([
+            {
+              name: 'name',
+              type: 'input',
+              message: 'Enter name',
+            },
+          ]);
+          users.remove(name.name);
+          break;
+        case Action.Update:
+          const updateUser = await inquirer.prompt([
+            {
+              name: 'name',
+              type: 'input',
+              message: 'Enter name of user you want to edit:',
+            },
+            {
+              name: 'newName',
+              type: 'input',
+              message: 'Enter updated name:',
+            },
+            {
+              name: 'newAge',
+              type: 'number',
+              message: 'Enter updated age:',
+            },
+          ]);
+          users.update(updateUser);
+          break;
+        case Action.Quit:
+          Message.showColorized(MessageVariant.Info, 'Bye bye!');
+          return;
+        default:
+          Message.showColorized(MessageVariant.Error, 'Command not found');
+          break;
+      }
+
       startApp();
     });
 };
